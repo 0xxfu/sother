698472860eef26fa2ebdcb96121fe0e40e000229
xfu <angerpeanut@gmail.com>
2023-06-12 20:39:50 +0800

    Update markdown printer

--- sother/sother/printers/markdown.py
+++ sother/sother/printers/markdown.py
@@ -8,6 +8,7 @@ import unittest
 from collections import OrderedDict
 from typing import Tuple, Type
 
+from slither.detectors.abstract_detector import classification_txt
 from slither.printers.abstract_printer import AbstractPrinter
 from slither.utils import output
 from slither.utils.output import Output
@@ -22,31 +23,46 @@ from sother.detectors import get_all_detector_wikis
 
 
 def _to_markdown(
-    detector_wiki: DetectorWiki, output_result: OutputResult
+    detector_wiki: DetectorWiki, output_results: list[OutputResult]
 ) -> Tuple[str, str]:
     """
     @return markdown: markdown string
     @return filename: source filename
     """
+    if len(output_results) <= 0:
+        return "", ""
     markdown = f"\n## {detector_wiki.wiki_title}\n"
     if detector_wiki.wiki_description or detector_wiki.wiki_exploit_scenario:
         markdown += f"\n### description:\n"
     if detector_wiki.wiki_description:
-        markdown += f"{detector_wiki.wiki_description}\n"
-    if output_result.markdown:
-        markdown += f"{output_result.markdown}\n"
+        markdown += f"{detector_wiki.wiki_description}\n\n"
+
+    if len(output_results) <= 1:
+        markdown += (
+            f"There is `{len(output_results)}` instance of this issue:\n"
+        )
+    else:
+        markdown += (
+            f"There are `{len(output_results)}` instances of this issue:\n"
+        )
+    for result in output_results:
+        markdown += f"{result.markdown}\n"
+
     if detector_wiki.wiki_exploit_scenario:
+        markdown += f"#### Exploit scenario\n"
         markdown += f"{detector_wiki.wiki_exploit_scenario}\n"
+
     markdown += f"\n### recommendation:\n"
     if detector_wiki.wiki_recommendation:
         markdown += f"{detector_wiki.wiki_recommendation}\n"
     markdown += f"\n### location:\n"
-    markdown += f"- {output_result.first_markdown_element}\n"
+    for result in output_results:
+        markdown += f"- {result.first_markdown_element}\n"
     markdown += f"\n### severity:\n"
-    markdown += f"{output_result.impact}\n"
+    markdown += f"{classification_txt[detector_wiki.impact]}\n"
     markdown += f"\n### category:\n"
-    markdown += f"{output_result.check}\n"
-    return markdown, output_result.get_first_element_file()
+    markdown += f"{detector_wiki.argument}\n"
+    return markdown, detector_wiki.argument
 
 
 class Markdown(AbstractPrinter):
@@ -59,31 +75,34 @@ class Markdown(AbstractPrinter):
         detector_results: list[list[dict]] = self.slither.run_detectors()
         detector_wikis = get_all_detector_wikis()
 
-        markdown_files: dict[str, str] = dict()
+        detector_outputs: dict[str, list[OutputResult]] = dict()
         for result in detector_results:
             for detector in result:
                 output_result = OutputResult(**detector)
-                wiki = (
-                    detector_wikis[output_result.check]
-                    if output_result.check in detector_wikis
-                    else None
-                )
-                (
-                    markdown_str,
-                    file_path,
-                ) = _to_markdown(wiki, output_result)
-                if file_path in markdown_files:
-                    markdown_files[file_path] += markdown_str
+                if output_result.check in detector_outputs:
+                    detector_outputs[output_result.check].append(output_result)
                 else:
-                    markdown_files[file_path] = markdown_str
+                    detector_outputs[output_result.check] = [output_result]
+        output_markdown = ""
+        for detector_check in detector_outputs:
+            print(detector_check, len(detector_outputs[detector_check]))
+            wiki = (
+                detector_wikis[detector_check]
+                if detector_check in detector_wikis
+                else None
+            )
+            (
+                markdown_str,
+                file_path,
+            ) = _to_markdown(wiki, detector_outputs[detector_check])
+            output_markdown += markdown_str
 
         res = self.generate_output(info)
-        for md_file in markdown_files:
-            file_path = f"{md_file}.audit.md"
-            self.info(f" export markdown file -> {file_path}")
-            with open(file_path, "w", encoding="utf8") as f:
-                f.write(markdown_files[md_file])
-            res.add_file(file_path, markdown_files[md_file])
+        file_path = f"{filename}_audit.md"
+        self.info(f" export markdown file -> {file_path}")
+        with open(file_path, "w", encoding="utf8") as f:
+            f.write(output_markdown)
+        res.add_file(file_path, output_markdown)
 
         return res
 
