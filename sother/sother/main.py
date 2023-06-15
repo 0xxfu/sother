c18bf68a28b82ce72fe179b5897bed8a4dbcc44f
xfu <angerpeanut@gmail.com>
2023-06-15 11:13:13 +0800

    Set logger level

--- sother/sother/main.py
+++ sother/sother/main.py
@@ -3,10 +3,12 @@
 @email: angerpeanut@gmail.com
 @date: 2023-06
 """
+import os
 import sys
 import unittest
 from typing import Type, Tuple, List
 
+from loguru import logger
 from slither.__main__ import main_impl
 from slither.detectors.abstract_detector import AbstractDetector
 from slither.printers.abstract_printer import AbstractPrinter
@@ -24,9 +26,19 @@ def get_detectors_and_printers() -> (
     return get_all_detectors(), get_all_printers()
 
 
+def guru_logger():
+    log_level = os.environ.get("LOGURU_LEVEL") or os.environ.get(
+        "loguru_level"
+    )
+    if not log_level:
+        logger.remove(0)
+        logger.add(sys.stdout, level="INFO")
+
+
 def start() -> (
     Tuple[List[Type[AbstractDetector]], List[Type[AbstractPrinter]]]
 ):
+    guru_logger()
     # Codebase with complex domninators can lead to a lot of SSA recursive call
     sys.setrecursionlimit(1500)
     detectors, printers = get_detectors_and_printers()
