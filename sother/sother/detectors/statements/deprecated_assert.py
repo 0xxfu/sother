0badf1d3def8e028152b7712e45ab5884fe299c1
xfu <angerpeanut@gmail.com>
2023-06-24 15:01:48 +0800

    Update deprecated_assert.py

--- sother/sother/detectors/statements/deprecated_assert.py
+++ sother/sother/detectors/statements/deprecated_assert.py
@@ -24,7 +24,13 @@ class DeprecatedAssert(AbstractDetector):
 
     WIKI_TITLE = "`revert CustomError()` should be used instead of `assert()`"
     WIKI_DESCRIPTION = """
-Prior to solidity version 0.8.0, hitting an assert consumes the **remainder of the transaction's available gas** rather than returning it, as `require()`/`revert()` do. `assert()` should be avoided even past solidity version 0.8.0 as its [documentation](https://docs.soliditylang.org/en/v0.8.19/control-structures.html#panic-via-assert-and-error-via-require) states that "The assert function creates an error of type Panic(uint256). ... Properly functioning code should never create a Panic, not even on invalid external input. If this happens, then there is a bug in your contract which you should fix.
+Prior to solidity version 0.8.0, hitting an assert consumes the **remainder of the 
+transaction's available gas** rather than returning it, as `require()`/`revert()` do. 
+`assert()` should be avoided even past solidity version 0.8.0 as its 
+[documentation](https://docs.soliditylang.org/en/latest/control-structures.html#panic-via-assert-and-error-via-require) 
+states that "The assert function creates an error of type Panic(uint256). ... 
+Properly functioning code should never create a Panic, not even on invalid external 
+input. If this happens, then there is a bug in your contract which you should fix.
 
 """
 
