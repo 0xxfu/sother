db1b17f1616a59ea862d3957a422f94820a15fc4
xfu <angerpeanut@gmail.com>
2023-06-24 15:54:13 +0800

    Update incorrect_solc.py
    TOO_RECENT_VERSION_TXT

--- sother/sother/detectors/attributes/incorrect_solc.py
+++ sother/sother/detectors/attributes/incorrect_solc.py
@@ -31,6 +31,18 @@ The recommendations take into account:
 Use a simple pragma version that allows any of these versions.
 Consider using the latest version of Solidity for testing."""
 
+    def _check_version(self, version: Tuple[str, str, str, str, str]) -> Optional[str]:
+        op = version[0]
+        if op and op not in [">", ">=", "^"]:
+            return self.LESS_THAN_TXT
+        version_number = ".".join(version[2:])
+        if version_number in self.BUGGY_VERSIONS:
+            return self.BUGGY_VERSION_TXT
+        if version_number not in self.ALLOWED_VERSIONS:
+            # remove check TOO_RECENT_VERSION_TXT
+            return self.OLD_VERSION_TXT
+        return None
+
 
 if __name__ == "__main__":
     unittest.main()
