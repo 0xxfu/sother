920041b15eee7958b8780d3f1e08d5703b817b65
xfu <angerpeanut@gmail.com>
2023-07-02 14:38:40 +0800

    Update smaller_uint_int.py

--- sother/sother/detectors/variables/smaller_uint_int.py
+++ sother/sother/detectors/variables/smaller_uint_int.py
@@ -35,7 +35,7 @@ class SmallerUintInt(AbstractDetector):
     WIKI_DESCRIPTION = """
 > When using elements that are smaller than 32 bytes, your contract’s gas usage may be higher. This is because the EVM operates on 32 bytes at a time. Therefore, if the element is smaller than that, the EVM must use more operations in order to reduce the size of the element from 32 bytes to the desired size.
 
-More detail see [this.](https://docs.soliditylang.org/en/v0.8.11/internals/layout_in_storage.html)
+More detail see [this.](https://docs.soliditylang.org/en/latest/internals/layout_in_storage.html)
 
 Each operation involving a `uint8` costs an extra [**22-28 gas**](https://gist.github.com/0xxfu/3672fec07eb3031cd5da14ac015e04a1) (depending on whether the other operand is also a variable of type `uint8`) as compared to ones involving `uint256`, due to the compiler having to clear the higher bits of the memory word before operating on the `uint8`, as well as the associated stack operations of doing so. Use a larger size then downcast where needed
 """
