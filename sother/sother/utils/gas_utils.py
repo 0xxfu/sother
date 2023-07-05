30d810491b7e817f93adc49239ba605e160503c5
xfu <angerpeanut@gmail.com>
2023-07-05 12:26:34 +0800

    Update gas_utils.py

--- sother/sother/utils/gas_utils.py
+++ sother/sother/utils/gas_utils.py
@@ -17,10 +17,6 @@ class GasUtils:
         result_functions: list["FunctionContract"] = []
         for contract in compilation_unit.contracts_derived:
             for function in contract.functions:
-                if function.visibility == "external" and (
-                    function.view or function.pure
-                ):
-                    continue
                 result_functions.append(function)
         return result_functions
 
