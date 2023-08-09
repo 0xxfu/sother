5b39cb9b07fa7786b3347f6b2056bc26c9c6311c
xfu <angerpeanut@gmail.com>
2023-08-09 11:24:02 +0800

    Update command_line.py

--- sother/utils/command_line.py
+++ sother/utils/command_line.py
@@ -12,8 +12,9 @@ from slither.detectors.abstract_detector import (
     classification_txt,
     DetectorClassification,
 )
+from slither.utils.colors import blue
 
-from sother.detectors import get_all_detectors
+from sother.detectors import get_all_detectors, get_detectors
 
 
 def output_detector_stats(detectors: list[Type[AbstractDetector]]) -> None:
@@ -23,14 +24,20 @@ def output_detector_stats(detectors: list[Type[AbstractDetector]]) -> None:
     for item in detectors:
         impact_detectors[item.IMPACT].append(item)
     impacts = sorted(impact_detectors.keys(), key=lambda x: x.value)
-
     for item in impacts:
         print(f"{classification_txt[item]}: {len(impact_detectors[item])}")
+    print(f"Total: {len(detectors)}")
 
 
 class CommandLineTestCase(unittest.TestCase):
     def test_stat_detectors(self):
-        output_detector_stats(get_all_detectors())
+        print(blue("------ All ------"))
+        detectors = get_all_detectors()
+        output_detector_stats(detectors)
+
+        print(blue("------ Sother ------"))
+        detectors = get_detectors()
+        output_detector_stats(detectors)
 
 
 if __name__ == "__main__":
